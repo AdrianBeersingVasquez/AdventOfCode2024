8c5176f6ca34d6e515d5a8a2d1e4e5ff6d7a4867
AdrianBeersingVasquez <57265495+AdrianBeersingVasquez@users.noreply.github.com>
2024-12-03 10:19:35 +0000

    day 2 part A

--- AdventOfCode2024/Day 1/day2/day2.py
+++ AdventOfCode2024/Day 1/day2/day2.py
@@ -5,17 +5,47 @@ Created on Mon Dec  2 09:10:09 2024
 @author: Adrian
 """
 
+'''
+CRITERIA
+
+- Criterion 1: Any two adjacent levels differ by at least one and at most three
+- Criterion 2: The levels are either all increasing or all decreasing.
+'''
+
 import numpy as np
-from functions import *
 
-df = pd.read_csv('test_data.txt', sep=' ', header=None)
+#arr = np.loadtxt('test_data.txt', dtype='int')
 
-print(df)
-print('\n')
-print(df.iloc[:,0:-1])
-print('\n')
-print(df.iloc[:,1:])
-diff = df.diff()
-diff = df.iloc[:,0:-1].subtract(df.iloc[:,1:])
+with open('input.txt', 'r') as f:
+    # Read each line and split it into a list of integers
+    data = [list(map(int, line.split())) for line in f]
 
+# Convert the list of lists into a NumPy array with dtype=object
+arr = np.array(data, dtype=object)
+
+print(arr[0,0])
+'''
+diff = arr[:,1:] - arr[:,0:-1] 
 print(diff)
+
+## Criteria 1
+# Remove any elements >2, and <-2
+arr1 = diff * (diff <= 2) * (diff >= -2)
+print('\n')
+print(arr1)
+
+# Change all 1s --> 2s
+arr1 = arr1 + arr1 * (arr1%2)
+print('\n')
+print(arr1)
+
+## Criteria 2
+diff2 = arr1[:,1:] + arr1[:,0:-1] 
+print('\n')
+print(diff2)
+    
+straightTrend = abs(np.sum(diff2, axis=1)) == 12
+
+totalSafe = np.sum(straightTrend)
+
+'''
