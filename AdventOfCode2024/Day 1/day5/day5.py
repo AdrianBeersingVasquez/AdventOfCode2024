195e4e567815ddcba359a0d99c26da9d1d686681
AdrianBeersingVasquez <57265495+AdrianBeersingVasquez@users.noreply.github.com>
2024-12-10 19:51:03 +0000

    Update day5.py

--- AdventOfCode2024/Day 1/day5/day5.py
+++ AdventOfCode2024/Day 1/day5/day5.py
@@ -5,14 +5,54 @@ Created on Thu Dec  5 11:37:27 2024
 @author: Adrian
 """
 
-def get_sample_data(file):
+def get_data(file):
+    
+    if file == 'input.txt':
+        split = 1176
+        
+    elif file == 'test_input.txt':
+        split = 21
+        
+    else:
+        print('use the test data or the input data ya eejit')
+    
     with open(file, 'r') as file:
         rules_and_updates = file.readlines()
     
-    rules = rules_and_updates[0:21]
-    updates = rules_and_updates[22:]
+    rules = rules_and_updates[0:split]
+    updates = rules_and_updates[split + 1:]
     
     return rules, updates
+
+def get_rules_subset(num, rules):
+    '''
+    Returns a subset of the rules containing every rule that includes num
+    '''
+    subset = []
+    
+    for rule in rules:
+        if num in rule:
+            num1 = int(rule[0]+rule[1])
+            num2 = int(rule[3]+rule[4])
+            subset.append([num1, num2])
+            
+    return subset
+
+def str_to_int(seqCSV):
+    '''
+    input a string of digits as strings, return a list of ints
+    '''
+    # if last char ',' (last number removed), remove final ','
+    if seqCSV[-1]==',':
+        seqCSV = seqCSV[:-1]
+        
+    # split by ','
+    sequence = seqCSV.split(',')
+    # convert to list of ints
+    sequence = [int(item) for item in sequence]
+    
+    return sequence
+
 '''
 
 For num in line, 
@@ -37,54 +77,38 @@ Get subRule
 # mid = len(line)// 2
 # cum_sum += mid
 11 22 33 44 55 66 77 88 99
-
 '''
 
-def get_rules_subset(num, rules):
-    
-    subset = []
-    
-    for rule in rules:
-        if num in rule:
-            num1 = int(rule[0]+rule[1])
-            num2 = int(rule[3]+rule[4])
-            subset.append([num1, num2])
-            
-    return subset
-
-file_path = 'input.txt'
-
-with open('input.txt', 'r') as file:
-    rules_and_updates = file.readlines()
-
-rules = rules_and_updates[0:1176] # 1175
-updates = rules_and_updates[1177:]
-
-rules, updates = get_sample_data('test_input.txt')
+# ('test_input.txt')
+# ('input.txt')
+rules, updates = get_data('test_input.txt')
 
 for line in updates:
     for char in range(0, len(line)-1, 3):
         num = line[char] + line[char+1]
         
-        print(char, num)
+        #print(char, num)
         
         miniRules = get_rules_subset(num, rules)
     #print(len(line))
 
-def str_to_int():
-    
-    
-    return 0
 
-n = 9
+n = 12
 print(line)
 print(line[:n] + line[n+3:])
-str1 = '75'
+str1 = '47'
 miniRules = get_rules_subset(num, rules)
 
-line_redacted = line[:n] + line[n+3:]
+comparisons = line[:n] + line[n+3:]
+comparisons = str_to_int(comparisons)
+print(comparisons)
+print(miniRules)
 
-line_redacted_int = str_to_int(line_redacted)
+for item in comparisons:
+    if item in miniRules:
+        print('Item in mini rules is', item)
+
+
 
 #for char2 in range(0)
 # ls_to_int
