9d7bf0b041e14a0a97da010fa1a8649cc58c945d
AdrianBeersingVasquez <57265495+AdrianBeersingVasquez@users.noreply.github.com>
2024-12-18 20:55:43 +0000

    Load input as np array

--- AdventOfCode2024/Day 1/day10.py
+++ AdventOfCode2024/Day 1/day10.py
@@ -7,5 +7,29 @@ Created on Wed Dec 18 19:47:28 2024
 
 import numpy as np
 
-arr = np.cs
+map_str = np.loadtxt('inputTest.txt', dtype='str')
+#map_str = np.loadtxt('input.txt', dtype='str')
+
+print(map_str)
+
+#print(arr)
+#print(len(arr))
+#print(len(arr[0]))
+heights = np.array([])
+
+for line in map_str:
+    line_int = np.array([])
+    
+    for char in line:
+        #print(char)
+        line_int = np.append(line_int, int(char))
+    
+    heights = np.append(heights, line_int, axis=0)
+
+heights = np.reshape(heights, (len(map_str), len(map_str[0])))
+
+
+#print(line_int)
+print(heights)
+
 
