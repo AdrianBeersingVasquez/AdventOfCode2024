d0b479afa41e8e27bac2225d712dd5918be8754f
AdrianBeersingVasquez <57265495+AdrianBeersingVasquez@users.noreply.github.com>
2024-12-18 19:52:36 +0000

    Day 10 start

---
+++ AdventOfCode2024/Day 1/day10.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 18 19:47:28 2024
+
+@author: Adrian
+"""
+
+import numpy as np
+
+arr = np.cs
+
---
+++ AdventOfCode2024/Day 1/day11/day11.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 12 17:51:17 2024
+
+@author: Adrian
+"""
+'''
+old_list = list of ints
+
+for i in blink:
+    new_list = []
+    
+    for number in seq:
+        
+        if old_list[i] == 0:
+            new_list.append(1)
+        
+        elif check2():
+        
+        
+        else:
+            new_list.append(old_list[i] * 2024)
+
+print(len(new_list))
+
+'''
