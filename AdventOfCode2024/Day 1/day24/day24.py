836bd6045f40d69e48430747bc88790d33ffc120
AdrianBeersingVasquez <57265495+AdrianBeersingVasquez@users.noreply.github.com>
2024-12-24 16:08:56 +0100

    Start of day 24

---
+++ AdventOfCode2024/Day 1/day24/day24.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 24 15:22:54 2024
+
+@author: beers
+"""
+import pandas as pd
+
+with open('input.txt', 'r') as file:
+    #data = file.readlines() # read()
+    data = file.read() # read()
+
+data = data.split('\n\n')
+
+starting_states = data[0].split('\n')
+states = {}
+for line in starting_states:
+    key, value = line.split(': ')
+    states[key] = value
+
+rules_ls = data[1].split('\n')
+rules = {}
+key=0
+for line in rules_ls:
+    print(line)
+    input1, operation, input2, _, output = line.split(' ')
+    rules[key] = ('input1': input1, 'operation': operation, 'input2': input2, 'output': output)
+    key+=1
